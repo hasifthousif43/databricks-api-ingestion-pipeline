8a00ab984081bdb38736280903935915f5791635
Athaulla Mulla <hasifthousif43@gmail.com>
2026-03-18 08:50:04 +0530

    Update 03_streaming_gold.py

--- notebooks/03_streaming_gold.py
+++ notebooks/03_streaming_gold.py
@@ -1,1 +1,15 @@
+from pyspark.sql.functions import *
 
+df = spark.readStream.format("delta") \
+    .load("/mnt/silver/api_data")
+
+agg_df = df.groupBy(
+    window(col("ingestion_time"), "5 minutes"),
+    col("userId")
+).count()
+
+agg_df.writeStream \
+    .format("delta") \
+    .outputMode("complete") \
+    .option("checkpointLocation", "/mnt/checkpoints/gold") \
+    .start("/mnt/gold/api_summary")
