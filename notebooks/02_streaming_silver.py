8f73414e85f680ff46ccf6e17c5b6e5f7b9a00cc
Athaulla Mulla <hasifthousif43@gmail.com>
2026-03-18 08:49:48 +0530

    Update 02_streaming_silver.py

--- notebooks/02_streaming_silver.py
+++ notebooks/02_streaming_silver.py
@@ -1,1 +1,14 @@
+from pyspark.sql.functions import *
 
+df = spark.readStream.format("delta") \
+    .load("/mnt/bronze/api_data")
+
+df_clean = df \
+    .withWatermark("ingestion_time", "10 minutes") \
+    .dropDuplicates(["id"])
+
+query = df_clean.writeStream \
+    .format("delta") \
+    .outputMode("append") \
+    .option("checkpointLocation", "/mnt/checkpoints/silver") \
+    .start("/mnt/silver/api_data")
