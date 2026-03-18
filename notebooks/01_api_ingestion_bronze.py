61558c46aa631113e06982c1dd7ebb2765da4783
Athaulla Mulla <hasifthousif43@gmail.com>
2026-03-18 08:49:24 +0530

    Update 01_api_ingestion_bronze.py

--- notebooks/01_api_ingestion_bronze.py
+++ notebooks/01_api_ingestion_bronze.py
@@ -1,1 +1,17 @@
+from pyspark.sql.functions import current_timestamp
+import json
 
+# Load config
+config = json.loads(dbutils.fs.head("/FileStore/configs/api_config.json"))
+
+from src.api_client import fetch_api_data
+
+data = fetch_api_data(config["api_url"])
+
+df = spark.createDataFrame(data)
+
+df = df.withColumn("ingestion_time", current_timestamp())
+
+df.write.format("delta") \
+    .mode("append") \
+    .save("/mnt/bronze/api_data")
